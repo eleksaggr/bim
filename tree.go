04b4decb8b28f0dce25868753e06efb3d42b71ec
Alex Egger <alex.egger96@gmail.com>
2016-08-23 15:30:29 +0200

    Remove old code from tree.go

--- tree.go
+++ tree.go
@@ -6,38 +6,6 @@ import (
 	"os"
 )
 
-// type treeMetadata struct {
-// 	Filename string
-// 	Mode     os.FileMode
-// 	Blob     Blob
-// }
-//
-// type Tree struct {
-// 	nodes []*Tree
-// 	Data  *treeMetadata
-// }
-//
-// func NewTree() *Tree {
-//
-// }
-//
-// func (tree Tree) Insert(filename string, mode os.FileMode, blob Blob) (t *Tree) {
-//
-// }
-//
-// func (tree Tree) Checksum() []byte {
-// 	checksum := make([]byte, sha1.Size)
-// 	h := sha1.New()
-// 	for _, node := range tree.nodes {
-// 		if node.nodes == nil {
-// 			checksum = h.Sum(node.Data.Blob)
-// 		} else {
-// 			checksum = h.Sum(node.Checksum())
-// 		}
-// 	}
-// 	return checksum
-// }
-
 var errNilTree = errors.New("Cannot operate on nil-tree.")
 
 type treeMeta struct {
