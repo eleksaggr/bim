6e82cc445554f5650c3181d1b856b839101687d6
Alex Egger <alex.egger96@gmail.com>
2016-08-23 16:35:25 +0200

    Add NewCommit method

--- commit.go
+++ commit.go
@@ -2,10 +2,14 @@ package bim
 
 import (
 	"crypto/sha1"
+	"errors"
 	"time"
 )
 
+var errCommitNoTree = errors.New("Cannot create commit without tree")
+
 type Commit struct {
+	id      [sha1.Size]byte
 	tree    *Tree
 	parents []*Commit
 
@@ -14,11 +18,35 @@ type Commit struct {
 	time   time.Time
 }
 
-func (commit Commit) Checksum() []byte {
-	var data []byte
-	data = append(data, commit.tree.Checksum()...)
-	data = append(data, []byte(commit.author)...)
-	data = append(data, []byte(commit.email)...)
+func NewCommit(author string, email string, tree *Tree, parents []*Commit) (commit *Commit, err error) {
+	if tree == nil {
+		return nil, errCommitNoTree
+	}
+	commit = &Commit{
+		author: author,
+		email:  email,
+		tree:   tree,
+		time:   time.Now(),
+	}
 
-	return sha1.New().Sum(data)
+	if parents == nil {
+		commit.parents = make([]*Commit, 0)
+	} else {
+		commit.parents = parents
+	}
+
+	commit.updateID()
+	return commit, nil
+}
+
+func (commit *Commit) updateID() {
+	h := sha1.New()
+	h.Write([]byte(commit.author))
+	h.Write([]byte(commit.email))
+	h.Write([]byte(commit.tree.id[:]))
+	temp := h.Sum(nil)
+
+	checksum := [sha1.Size]byte{}
+	copy(checksum[:], temp[0:20])
+	commit.id = checksum
 }
